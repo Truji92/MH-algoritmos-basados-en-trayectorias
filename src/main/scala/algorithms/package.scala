b1087a2671b22aa03abf379ed4afcc7ee7284fd6
Alejandro Trujillo Caballero <alejandro.trujillo.7@gmail.com>
2016-03-04 22:09:00 +0100

    Cálculo de coste

---
+++ src/main/scala/algorithms/package.scala
@@ -0,0 +1,14 @@
+import types.Types.{Solution, OptimalSolution, ProblemData}
+
+package object algorithms {
+
+  def cost(problem: ProblemData, solution: Solution) = {
+    val (nItems, m1, m2) = problem
+    val sumatory = for {
+      i <- 0 until nItems
+      j <- 0 until nItems
+    } yield m1(i)(j) * m2(solution(i)-1)(solution(j)-1)
+    sumatory.sum
+  }
+
+}
