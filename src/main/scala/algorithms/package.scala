2e084db1ec312a33e7b73db7d945dbdd3a95d53e
Alejandro Trujillo Caballero <alejandro.trujillo.7@gmail.com>
2016-03-28 00:42:27 +0200

    Return type annotation to fix editor bux

--- src/main/scala/algorithms/package.scala
+++ src/main/scala/algorithms/package.scala
@@ -2,7 +2,7 @@ import types.Types.{Solution, OptimalSolution, ProblemData}
 
 package object algorithms {
 
-  def cost(problem: ProblemData, solution: Solution) = {
+  def cost(problem: ProblemData, solution: Solution): Int = {
     val (nItems, m1, m2) = problem
     val sumatory = for {
       i <- 0 until nItems
