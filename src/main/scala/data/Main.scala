4d9b1d7229656e6b7b47e48dfb6f511e27a4e52e
Alejandro Trujillo Caballero <alejandro.trujillo.7@gmail.com>
2016-03-28 23:31:23 +0200

    Results

--- src/main/scala/data/Main.scala
+++ src/main/scala/data/Main.scala
@@ -8,18 +8,77 @@ import scala.util.Random
 
 object Main {
   def main(args: Array[String]) {
-    Problems.tai150 match {
-      case (inputs, solution) =>
-        println("*******************************************************************")
-        //println("Simulated Annealing:  " + cost(inputs, SimulatedAnnealing(inputs)))
-        //println("Local Search:         " + cost(inputs, LocalSearch(inputs)))
-        println("Tabu:                 " + cost(inputs, TabuSearch(inputs, new Random)))
-        println("Greedy:               " + cost(inputs, Greedy(inputs)))
-        //println("Random Search:        " + cost(inputs, RandomSearch(inputs)))
-        println("Optimal:              " + solution._2)
-        println("*******************************************************************")
-    }
+//    Problems.tai150 match {
+//      case (inputs, solution) =>
+//        println("*******************************************************************")
+//        //println("Simulated Annealing:  " + cost(inputs, SimulatedAnnealing(inputs)))
+//        //println("Local Search:         " + cost(inputs, LocalSearch(inputs)))
+//        println("Tabu:                 " + cost(inputs, TabuSearch(inputs, new Random)))
+//        println("Greedy:               " + cost(inputs, Greedy(inputs)))
+//        //println("Random Search:        " + cost(inputs, RandomSearch(inputs)))
+//        println("Optimal:              " + solution._2)
+//        println("*******************************************************************")
+//    }
+    generateTabuFiles()
     //generateTestingFiles()
+  }
+
+  def generateTabuFiles() = {
+    //================================================
+    var pw = new PrintWriter(new File("Tabu150" ))
+    println("=== 150 ===")
+    pw.write("SEED, TABUSEARCH \n")
+
+    for (i <- 1 to 10) {
+      val (inputs, _) = Problems.tai150
+
+      val tabu = TabuSearch(inputs, new Random(i))
+      pw.write(", " + cost(inputs, tabu) + "\n")
+
+      println(i)
+    }
+
+    pw.close()
+    //================================================
+
+
+    //================================================
+    pw = new PrintWriter(new File("Tabu90" ))
+    println("=== 90 ===")
+    pw.write("SEED, LOCALSEARCH, RANDOMSEARCH, SIMANNEALING \n")
+
+    for (i <- 1 to 10) {
+      val (inputs, _) = Problems.sko90
+
+      val tabu = TabuSearch(inputs, new Random(i))
+      pw.write(", " + cost(inputs, tabu) + "\n")
+
+      println(i)
+
+    }
+
+    pw.close()
+    //================================================
+
+
+
+    //================================================
+    pw = new PrintWriter(new File("Tabu25" ))
+    println("=== 25 ===")
+    pw.write("SEED, LOCALSEARCH, RANDOMSEARCH, SIMANNEALING \n")
+
+    for (i <- 1 to 10) {
+      val (inputs, _) = Problems.tai25
+
+      val tabu = TabuSearch(inputs, new Random(i))
+      pw.write(", " + cost(inputs, tabu) + "\n")
+
+      println(i)
+    }
+
+    pw.close()
+    //================================================
+
   }
 
   def generateTestingFiles() = {
